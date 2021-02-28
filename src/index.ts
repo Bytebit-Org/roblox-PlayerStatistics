991c3681cee26a123289605e6ec0650555cd06af
Noah Crowley <noahwillcrow@gmail.com>
2021-02-27 17:41:32 -0800

    export new interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 export * from "classes/PersistenceLayers/DataStorePlayerStatisticsPersistenceLayer";
 export * from "classes/PlayerStatisticsProvider";
 
+export { IPlayerStatisticEventsPoster } from "interfaces/IPlayerStatisticEventsPoster";
+export { IPlayerStatisticsLoader } from "interfaces/IPlayerStatisticsLoader";
 export { IPlayerStatisticsPersistenceLayer } from "interfaces/IPlayerStatisticsPersistenceLayer";
+export { IPlayerStatisticsProvider } from "interfaces/IPlayerStatisticsProvider";
+export { IPlayerStatisticsReader } from "interfaces/IPlayerStatisticsReader";
 
 export { EventsDefinition } from "types/EventsDefinition";
 export { StatisticsDefinition } from "types/StatisticsDefinition";
