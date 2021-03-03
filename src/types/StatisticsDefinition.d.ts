2801de7aa586087128f5e9e89e217429b4bd4ddd
Noah Crowley <noahwillcrow@gmail.com>
2021-03-02 20:14:27 -0800

    Update types and write out README.md

--- src/types/StatisticsDefinition.d.ts
+++ src/types/StatisticsDefinition.d.ts
@@ -1,12 +1,8 @@
+import { StatisticDescription } from "./StatisticDescription";
+
 /**
  * Defines a list of statistics, keyed by their string name
  */
 export type StatisticsDefinition = {
-	readonly [key: string]: {
-		/** The default value to use for the statistic */
-		readonly defaultValue: number;
-
-		/** A function to update the statistic value given the value of a single event and the previous value for the statistic */
-		readonly updateFunction: (eventValue: number, previousStatisticValue: number) => number;
-	};
+	readonly [key: string]: StatisticDescription;
 };
